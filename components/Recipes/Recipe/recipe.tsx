d65c9a055ce37a5866e3da9d3cd3a22c99b921ba
Pratap Reddy <pratapreddy15@gmail.com>
2021-12-25 11:30:55 +1100

    adding spinner to card when image is being loaded

--- components/Recipes/Recipe/recipe.tsx
+++ components/Recipes/Recipe/recipe.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 
+import { ImageLoader } from '../../'
 import classes from './recipe.module.css'
 
 interface RecipeProps {
@@ -12,10 +14,21 @@ interface RecipeProps {
 }
 
 function Recipe(props: RecipeProps) {
+  const [imageLoaded, setImageLoaded] = useState(false)
+
   return (
     <li className={classes.item}>
       <div className={classes.photo}>
-        <Image objectFit="cover" className={classes.image} src={props.imageSource} layout="fill" alt={props.name} />
+        {!imageLoaded && <ImageLoader />}
+        <Image
+          objectFit="cover"
+          className={classes.image}
+          src={props.imageSource}
+          layout="fill"
+          alt={props.name}
+          onLoadingComplete={(e) => setImageLoaded(true)}
+        />
+        {/* <ImageLoader /> */}
       </div>
       <div className={classes.footer}>
         <Link href={props.url}>
