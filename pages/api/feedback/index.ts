ba6d0cbf24b4cb190dbae841054b0ed8c2d60100
Pratap Reddy <pratapreddy15@gmail.com>
2021-12-26 19:05:01 +1100

    log feedback to console

--- pages/api/feedback/index.ts
+++ pages/api/feedback/index.ts
@@ -3,9 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { name, email, message } = JSON.parse(req.body)
-    console.log('Name', name)
-    console.log('Email', email)
-    console.log('Message', message)
+    console.log(`Message received from ${name} (${email})`, message)
 
     res.status(201).json({})
   }
